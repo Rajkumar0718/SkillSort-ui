a725a63c9a27f65d0d8716100ccff993cea48d6f
pasupathi1817 <pasupathiselvam5@gmail.com>
2024-01-23 15:12:59 +0530

    test admin changes push

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import AdminLogin from "./components/Admin/AdminLogin";
-import { Route, Routes, useRoutes } from "react-router-dom";
-import HomePage from "./components/SuperAdmin/HomePage";
-import PageNotFound from "./components/PageNotFound";
-import CollegeList from "./components/SuperAdmin/CollegeList";
-import CollegeLayout from "./container/CollegeLayout";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./common/Layout";
-import StudentList from "./components/college/StudentList";
+import AdminLogin from "./components/Admin/AdminLogin";
+import PageNotFound from "./components/PageNotFound";
 import RequireAuth from "./components/RequireAuth";
-import StaffList from "./components/college/StaffList";
+import AddQuestion from "./components/TestAdmin/AddQuestion";
 import AddStaff from "./components/college/AddStaff";
 import CollegeReportList from "./components/college/CollegeReportList";
-import AddStudent from "./components/college/AddStudent";import AddQuestion from "./components/TestAdmin/AddQuestion";
+import StaffList from "./components/college/StaffList";
+import StudentList from "./components/college/StudentList";
+import  './assests/css/AdminDashboard.css';
+import './assests/css/SuperAdminDashboard.css';
+import Dashboard from "./components/TestAdmin/Dashboard";
+import SectionList from "./components/TestAdmin/SectionList";
+import Question from "./components/TestAdmin/Question";
+import SettingList from "./components/Admin/SettingList";
+import GroupTypesList from "./components/TestAdmin/GroupTypesList";
+
 
 function App() {
   return (
@@ -31,8 +36,14 @@ function App() {
           <Route path="/college/collegeReport" element={<CollegeReportList/>} />        
         </Route>
         <Route element={<RequireAuth allowedRoles={["TEST_ADMIN"]}/>}>
-          <Route path="/testadmin" element={<AddQuestion />} />
-                 
+           <Route path="/testadmin/dashboard"  name= 'Dashboard'  element={<Dashboard />} />
+           <Route path="/testadmin/section"  name= 'Sections'  element={<SectionList />} />
+           <Route path="/testadmin/question"  name= 'Questions'  element={<Question />} />
+           <Route path="/testadmin/question/add"  name= 'Add Question'  element={<AddQuestion />} />
+           <Route path="/testadmin/question/edit"  name= 'Edit Question'  element={<AddQuestion />} />
+           <Route path="/testadmin"  name= 'Test List'  element={<Question />} />
+           <Route path="/testadmin/setting"  name= 'Setting List'  element={<SettingList />} />
+           <Route path="/testadmin/grouptypes"    element={<GroupTypesList />} />
         </Route>
 
         <Route path="*" element={<PageNotFound />} />
@@ -42,3 +53,14 @@ function App() {
 }
 
 export default App;
+// { path: '/testadmin/dashboard', name: 'Dashboard', component: Dashboard },
+// { path: '/testadmin/section', name: 'Sections', component: SectionList },
+// { path: '/testadmin/question', name: 'Questions', component: QuestionList },
+// { path: '/testadmin/question/add', name: 'Add Question', component: AddQuestion },
+// { path: '/testadmin/question/edit', name: 'Edit Question', component: AddQuestion },
+// { path: '/testadmin', name: "Test List", component: QuestionList },
+// { path: '/testadmin/add', name: 'Add Test', component: AddExam },
+// { path: '/testadmin/edit', name: 'Edit Test', component: AddExam },
+// { path: '/testadmin/setting', name: 'Setting List', component: SettingList },
+// { path: "/testadmin/smtp", name: 'SMTP', component: SMTPConfig },
+// { component: PageNotFound }
