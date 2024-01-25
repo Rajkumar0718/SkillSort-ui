903df655f802ac8165eef508a941676ce94ada4a
pasupathi1817 <pasupathiselvam5@gmail.com>
2024-01-25 18:58:35 +0530

    testadmin route update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './assests/css/AdminDashboard.css';
+import './assests/css/SuperAdminDashboard.css';
 import Layout from "./common/Layout";
 import AdminLogin from "./components/Admin/AdminLogin";
 import PageNotFound from "./components/PageNotFound";
 import RequireAuth from "./components/RequireAuth";
-import AddQuestion from "./components/TestAdmin/AddQuestion";
-import AddStaff from "./components/college/AddStaff";
-import CollegeReportList from "./components/college/CollegeReportList";
-import AddStudent from "./components/college/AddStudent";
-import ListHr from "./components/Admin/ListHr";
-import AddHr from "./components/Admin/AddHr";
-import OnGoingExam from "./components/Admin/OnGoingExam";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import SectionList from "./components/TestAdmin/SectionList"
+import Dashboard from "./components/TestAdmin/Dashboard"
+import Question from "./components/TestAdmin/Question"
+import AddQuestion from "./components/TestAdmin/AddQuestion"
+import GroupTypesList from "./components/TestAdmin/GroupTypesList"
+import SettingList from "./components/Admin/SettingList"
 function App() {
   return (
     <>
@@ -22,40 +22,27 @@ function App() {
       <Route path="/login" element={<AdminLogin />} />
       <Route path="/" element={<Layout />}>
         {/* Protected Routes */}
-        <Route
-          element={
-            <RequireAuth allowedRoles={["COLLEGE_ADMIN", "COLLEGE_STAFF"]} />
-          }
-        >
-          <Route path="/college" element={<StudentList />} />
-          <Route
-            path="/college/placement-coordinator"
-            element={<StaffList />}
-          />
-          <Route
-            path="/college/placement-coordinator/add"
-            element={<AddStaff />}
-          />
-          <Route
-            path="/college/collegeReport"
-            element={<CollegeReportList />}
-          />
-          <Route path="/college/add" element={<AddStudent />} />
-        </Route>
+        
         <Route 
           element={
-            <RequireAuth allowedRoles={["ADMIN"]} />
+            <RequireAuth allowedRoles={["TEST_ADMIN"]} />
           }
         >  
-          <Route path="/admin/hr" element={<ListHr />} />
-          <Route path="/admin/hr/add" element={<AddHr />} />
-          <Route path="/admin/hr/edit" element={<AddHr />} />
-          <Route path="/admin/onGoingTest" element={<OnGoingExam />} />
+          <Route path="/testadmin/dashboard" element={<Dashboard />} />
+          <Route path="/testadmin/dashboard" element={<Dashboard />} />
+           <Route path='/testadmin/section' element={<SectionList />} />
+           <Route path='/testadmin/question' element={<Question />} />
+           <Route path='/testadmin/question/add' element={<AddQuestion />} />
+           <Route path='/testadmin/question/edit' element={<AddQuestion />} />
+           <Route path='/testadmin/grouptypes' element={<GroupTypesList />} />
+           <Route path='/testadmin/setting' element={<SettingList />} />
+
+
+
+
 
         </Route>
-
         <Route path="*" element={<PageNotFound />} />
-        
       </Route>
       
     </Routes>
