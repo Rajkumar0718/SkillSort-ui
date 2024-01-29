97cbca85702a8c01bc319623cecac5e8d85f0681
Rajkumar0718 <rajkumar071810@gmail.com>
2024-01-29 12:20:36 +0530

    commited upto changed

--- src/components/Admin/ExamMailModel.js
+++ src/components/Admin/ExamMailModel.js
@@ -11,46 +11,50 @@ export default class ExamMailModel extends Component {
   state = {
     loader: false,
     validMailButton: false,
-    id: '',
-    mailMsg: '',
+    id: "",
+    mailMsg: "",
     selectedFile: null,
     validUploadButton: true,
-    shortListedCandidates:[],
+    shortListedCandidates: [],
     csvImportObject: {
-      section: '',
-      questionType: '',
+      section: "",
+      questionType: "",
       difficulty: "",
-      examType: '',
+      examType: "",
       mailOpenModal: false,
       mailModalSection: "",
     },
-    questionType: ['MCQ', 'True/False', 'programming'],
-    examType: ['MOCK', 'ACTUAL']
-  }
+    questionType: ["MCQ", "True/False", "programming"],
+    examType: ["MOCK", "ACTUAL"],
+  };
 
   componentWillMount() {
     if (this.props.modalSection.type === "Question") {
       return null;
     }
     if (this.props.modalSection.type === "Email") {
-      if(this.props.mailModalSection?.exam){
+      if (this.props.mailModalSection?.exam) {
         this.setState({ mailMsg: this.props.mailModalSection?.exam?.message });
-      }
-      else this.getExam(this.props.examId)
+      } else this.getExam(this.props.examId);
     }
-    if(this.props.shortListedCandidates){
-      this.setState({shortListedCandidates:this.props.shortListedCandidates})
+    if (this.props.shortListedCandidates) {
+      this.setState({
+        shortListedCandidates: this.props.shortListedCandidates,
+      });
     }
   }
 
-  getExam =  (examId) => {
-		axios.get(`${url.CANDIDATE_API}/candidate/exam/instruction?examId=${examId}`, { headers: authHeader() }
-		).then(res =>{
-			const exam = res.data.response
-      this.setState({ mailMsg: exam.message,exam:exam});
-		})
-		  .catch(() => toastMessage('error', 'Error while fetching exam'))
-	  }
+  getExam = (examId) => {
+    axios
+      .get(`${url.CANDIDATE_API}/candidate/exam/instruction?examId=${examId}`, {
+        headers: authHeader(),
+      })
+      .then((res) => {
+        const exam = res.data.response;
+        this.setState({ mailMsg: exam.message, exam: exam });
+      })
+      .catch(() => toastMessage("error", "Error while fetching exam"));
+  };
 
   sendExamLink = (data) => {
     this.handleMailSentEventTrack()
@@ -58,20 +62,27 @@ export default class ExamMailModel extends Component {
     data.positionId = this.props.positionId;
     this.setState({ validMailButton: true, loader: true });
     let formData = new FormData();
-    formData.append("file",this.state.selectedFile)
-    formData.append("exam",JSON.stringify(data))
-    formData.append("candidateEmails",_.map(this.state.shortListedCandidates,s=>s.id))
-    axios.post(`${url.ADMIN_API}/onlineTest/send/link`, formData, { headers: authHeader()})
-      .then(_res => {
+    formData.append("file", this.state.selectedFile);
+    formData.append("exam", JSON.stringify(data));
+    formData.append(
+      "candidateEmails",
+      _.map(this.state.shortListedCandidates, (s) => s.id)
+    );
+    axios
+      .post(`${url.ADMIN_API}/onlineTest/send/link`, formData, {
+        headers: authHeader(),
+      })
+      .then((_res) => {
         this.setState({ validMailButton: false, loader: false });
-        toastMessage('success', "Emails sent Successfully..!");
-        this.props.onCloseModal()
+        toastMessage("success", "Emails sent Successfully..!");
+        this.props.onCloseModal();
         this.setState({ mailOpenModal: true });
-      }).catch(error => {
+      })
+      .catch((error) => {
         this.setState({ validMailButton: false, loader: false });
         if (error.response?.status === 401) {
           logOut();
-          this.props.history.push('/login');
+          this.props.history.push("/login");
         }
         errorHandler(error);
       })
@@ -83,45 +94,77 @@ export default class ExamMailModel extends Component {
     });
   }
 
-  onFileChange = event => {
-    this.setState({ selectedFile: event.target.files[0], validUploadButton: false });
+  onFileChange = (event) => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      validUploadButton: false,
+    });
   };
 
   importSampleFile = () => {
     // eslint-disable-next-line default-case
     switch (this.props.modalSection.type) {
-      case 'Email':
-        saveAs(new Blob(['S.NO', ',', 'EMAIL'], { type: "text/plain" }), `candidate_email.csv`);
+      case "Email":
+        saveAs(
+          new Blob(["S.NO", ",", "EMAIL"], { type: "text/plain" }),
+          `candidate_email.csv`
+        );
         break;
-      case 'Student':
-        saveAs(new Blob(['S.NO', ',', 'FIRST_NAME', ',', 'LAST_NAME', ',', 'EMAIL', ',', 'PHONE'], { type: "text/plain" }), 'student.csv');
+      case "Student":
+        saveAs(
+          new Blob(
+            [
+              "S.NO",
+              ",",
+              "FIRST_NAME",
+              ",",
+              "LAST_NAME",
+              ",",
+              "EMAIL",
+              ",",
+              "PHONE",
+            ],
+            { type: "text/plain" }
+          ),
+          "student.csv"
+        );
         break;
-      case 'Question':
+      case "Question":
         let header;
-        if (this.state.csvImportObject.questionType === 'MCQ') {
-          header = ['SECTION,QUESTION TYPE,EXAM TYPE,DIFFICULTY,QUESTION,ANSWER,OPTION-A,OPTION-B,OPTION-C,OPTION-D,OPTION-E'];
+        if (this.state.csvImportObject.questionType === "MCQ") {
+          header = [
+            "SECTION,QUESTION TYPE,EXAM TYPE,DIFFICULTY,QUESTION,ANSWER,OPTION-A,OPTION-B,OPTION-C,OPTION-D,OPTION-E",
+          ];
         } else {
-          header = ['SECTION,QUESTION TYPE,EXAM TYPE,DIFFICULTY,QUESTION,ANSWER,OPTION-A,OPTION-B'];
+          header = [
+            "SECTION,QUESTION TYPE,EXAM TYPE,DIFFICULTY,QUESTION,ANSWER,OPTION-A,OPTION-B",
+          ];
         }
-        let str = '';
-        let row = 'S.NO,';
-        let line = '';
+        let str = "";
+        let row = "S.NO,";
+        let line = "";
         for (let index in header) {
-          row += header[index].toString().toUpperCase() + ',';
+          row += header[index].toString().toUpperCase() + ",";
         }
         row = row.slice(0, -1);
-        str += row + '\r\n';
-        line = '1,' + this.state.csvImportObject.section + ',' + this.state.csvImportObject.questionType + ',' + this.state.csvImportObject.examType;
-        str += line + '\r\n';
+        str += row + "\r\n";
+        line =
+          "1," +
+          this.state.csvImportObject.section +
+          "," +
+          this.state.csvImportObject.questionType +
+          "," +
+          this.state.csvImportObject.examType;
+        str += line + "\r\n";
         saveAs(new Blob([str], { type: "text/plain" }), `question.csv`);
         break;
     }
-  }
+  };
   handleChange = (e, key) => {
     const { csvImportObject } = this.state;
     csvImportObject[key] = e.target.value;
     this.setState({ csvImportObject });
-  }
+  };
 
   renderQuestionTemplate = () => {
     // eslint-disable-next-line default-case
@@ -162,76 +205,122 @@ export default class ExamMailModel extends Component {
             <button className='btn btn-sm btn-nxt' disabled={(this.state.csvImportObject?.section !== '' && this.state.csvImportObject?.questionType !== '') ? false : true} onClick={this.importSampleFile} ><i className="fa fa-download" aria-hidden="true"></i> Sample Template</button>
             <strong className='ml-2' style={{ color: '#3b489e' }}>( *must upload this file format )</strong>
           </div>
-        </div>
-      case 'Email':
-        return <button className='btn btn-sm btn-nxt' style={{ marginLeft: "5px" }} onClick={this.importSampleFile} ><i className="fa fa-download" aria-hidden="true"></i> Sample Template</button>
+        );
+      case "Email":
+        return (
+          <button
+            className="btn btn-sm btn-nxt"
+            style={{ marginLeft: "20px" }}
+            onClick={this.importSampleFile}
+          >
+            <i className="fa fa-download" aria-hidden="true"></i> Sample
+            Template
+          </button>
+        );
 
-      case 'Student':
-        return <button className='btn btn-sm btn-nxt' style={{ marginLeft: "5px" }} onClick={this.importSampleFile} ><i className="fa fa-download" aria-hidden="true"></i> Sample Template</button>
+      case "Student":
+        return (
+          <button
+            className="btn btn-sm btn-nxt"
+            style={{ marginLeft: "20px" }}
+            onClick={this.importSampleFile}
+          >
+            <i className="fa fa-download" aria-hidden="true"></i> Sample
+            Template
+          </button>
+        );
     }
-  }
+  };
 
   onFileUpload = () => {
     this.setState({ validUploadButton: true, loader: true });
     // Create an object of formData
     const formData = new FormData();
     // Update the formData object
-    formData.append('file', this.state.selectedFile);
+    formData.append("file", this.state.selectedFile);
     // eslint-disable-next-line default-case
     switch (this.props.modalSection.type) {
-      case 'Email':
-        formData.append('examId', this.props.modalSection?.exam?.id);
-        axios.post(` ${url.ADMIN_API}/examUsers/upload`, formData, {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'multipart/form-data'
-          }
-        }).then(res => {
-          this.setState({ loader: false });
-          this.setState({ mailOpenModal: true });
-          toastMessage('success', res.data.message);
-        }).catch(error => {
-          this.setState({ validUploadButton: false, loader: false });
-          toastMessage('error', error.response?.data?.message);
-        })
+      case "Email":
+        formData.append("examId", this.props.modalSection?.exam?.id);
+        axios
+          .post(` ${url.ADMIN_API}/examUsers/upload`, formData, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((res) => {
+            this.setState({ loader: false });
+            this.setState({ mailOpenModal: true });
+            toastMessage("success", res.data.message);
+          })
+          .catch((error) => {
+            this.setState({ validUploadButton: false, loader: false });
+            toastMessage("error", error.response?.data?.message);
+          });
         break;
-      case 'Question':
-        axios.post(` ${url.ADMIN_API}/question/bulk/question/`, formData, {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'multipart/form-data'
-          }
-        }).then(res => {
-          this.setState({ loader: false });
-          this.props.onCloseModal();
-          toastMessage('success', "Questions uploaded successfully..!!");
-        }).catch(error => {
-          this.setState({ validUploadButton: false, loader: false });
-          toastMessage('error', error);
-        })
+      case "Question":
+        axios
+          .post(` ${url.ADMIN_API}/question/bulk/question/`, formData, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((res) => {
+            this.setState({ loader: false });
+            this.props.onCloseModal();
+            toastMessage("success", "Questions uploaded successfully..!!");
+          })
+          .catch((error) => {
+            this.setState({ validUploadButton: false, loader: false });
+            toastMessage("error", error);
+          });
         break;
-      case 'Student':
-        axios.post(` ${url.COLLEGE_API}/student/bulk/save`, formData, {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'multipart/form-data'
-          }
-        }).then(res => {
-          this.setState({ loader: false });
-          this.props.onCloseModal();
-          toastMessage('success', res.data.message);
-        }).catch(error => {
-          this.setState({ validUploadButton: false, loader: false });
-          toastMessage('error', error.response?.data?.message || 'Something went wrong while uploading')
-        })
+      case "Student":
+        axios
+          .post(` ${url.COLLEGE_API}/student/bulk/save`, formData, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((res) => {
+            this.setState({ loader: false });
+            this.props.onCloseModal();
+            toastMessage("success", res.data.message);
+          })
+          .catch((error) => {
+            this.setState({ validUploadButton: false, loader: false });
+            toastMessage(
+              "error",
+              error.response?.data?.message ||
+                "Something went wrong while uploading"
+            );
+          });
     }
   };
-
+  handleEditorChange = (event, editor) => {
+    const data = editor.getData();
+    this.setState({ mailMsg: data });
+  };
 
   render() {
-    const questionType = this.props.modalSection.type === 'Email';
+    const questionType = this.props.modalSection.type === "Email";
     return (
-      <div className="modal fade show" id="myModal" role="dialog" style={{ paddingRight: '15px', display: 'flex', justifyContent: "center", alignItems: "center", backgroundColor: 'rgba(0,0,0,0.90)' }} aria-hidden="true">
+      <div
+        className="modal fade show"
+        id="myModal"
+        role="dialog"
+        style={{
+          paddingRight: "15px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(0,0,0,0.90)",
+        }}
+        aria-hidden="true"
+      >
         {fallBackLoader(this.state.loader)}
         <div className="modal-dialog" style={{ width: "700px", maxWidth: "770px" }}>
           <div className="modal-content" style={{ borderStyle: 'solid', borderColor: '#af80ecd1', borderRadius: "32px" }}>
@@ -240,7 +329,7 @@ export default class ExamMailModel extends Component {
               {this.props.modalSection?.type === "Email" ? <h5 className="setting-title" style={{ paddingLeft: '0.5rem' }}><span style={{ color: '#F05A28' }}>{this.props.remainingTest}</span>   Credits Available</h5> : ''}
               <button type="button" onClick={this.props.onCloseModal} className="close" data-dismiss="modal">&times;</button>
             </div>
-            <div className='card-body' style={{ paddingTop: "10px" }}>
+            <div className="card-body" style={{ paddingTop: "4px" }}>
               <div>
                 {this.renderQuestionTemplate()}
                 {(this.props.mailModalSection?.exam===null && this.props.modalSection.type === "Email") || this.props.modalSection?.type === "Student"? <strong className='ml-2' style={{ color: '#3b489e' }}>( *must upload this file format )</strong> : this.props.modalSection?.type !== "Question" && this.props.modalSection?.type !== "Student"  ?  <strong className='ml-2' style={{ color: '#3b489e' }}>( *You can add more candidates, but the file must be in CSV format )</strong> : null}
@@ -288,8 +377,8 @@ export default class ExamMailModel extends Component {
                     <div className="col-md-11" style={{ display: "flex",justifyContent: "flex-end" }}>
                       <button disabled={!this.state.selectedFile && this.props.mailModalSection?.exam===null } type="button" onClick={() => this.sendExamLink(this.props.mailModalSection.exam || this.state.exam)} className="btn btn-sm btn-nxt" >Send Mail</button>
                     </div>
-                  </div>
-                </form>}
+                  </form>
+                )}
               </div>
             </div>
           </div>
