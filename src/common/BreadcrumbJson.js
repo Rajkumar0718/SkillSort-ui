8275ce5eb684ff79c4940478bb90760a276d59e4
MUTHU KRISHNAN R <73745068+kishore0995@users.noreply.github.com>
2024-01-29 12:12:10 +0530

    College UI (#4)
    College-UI completed

--- src/common/BreadcrumbJson.js
+++ src/common/BreadcrumbJson.js
@@ -1,5 +1,6 @@
 const breadcrumb = {
-  COLLEGE_ADMIN: [{ homeLink: "/college", name: "college" }],
-  SUPER_ADMIN: [{ homeLink: '/home', name: 'home' }]
+  COLLEGE_ADMIN: [ { homeLink: "/college", name: "college" }],
+  SUPER_ADMIN: [{ homeLink: '/home', name: 'home' }],
+  ADMIN:[{homeLink: "admin/dashboard", name: "admin"}]
 };
 export default breadcrumb;
