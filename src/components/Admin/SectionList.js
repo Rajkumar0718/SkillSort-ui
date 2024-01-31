d28217b8614735a4327d374de6f638c7bbba915c
pasupathi1817 <pasupathiselvam5@gmail.com>
2024-01-31 11:07:15 +0530

    admin section list added

--- src/components/Admin/SectionList.js
+++ src/components/Admin/SectionList.js
@@ -1,148 +1,58 @@
 import axios from "axios";
 import _ from "lodash";
-import React, { Component } from "react";
+import React, { Component,useState ,useRef ,useEffect } from "react";
 import { authHeader, errorHandler } from "../../api/Api";
-import { fallBackLoader } from '../../utils/CommonUtils';
+import { CustomTable } from "../../utils/CustomTable";
 import Pagination from "../../utils/Pagination";
-import SectionModal from "./SectionModal";
-import url from "../../utils/UrlConstant";
-import { Link } from "react-router-dom";
-import { CustomTable } from '../../utils/CustomTable';
-import { MenuItem } from '@mui/material';
+import SectionModal from "./AddSectionModal";
 import CustomMenuItem from "../../utils/Menu/CustomMenuItem";
-export default class SectionList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sections: [],
-      openModal: false,
-      openModalAdd: false,
-      modalSection: "",
-      status: "ACTIVE",
-      loader: false,
-      currentPage: 1,
-      pageSize: 10,
-      totalPages: 0,
-      totalElements: 0,
-      numberOfElements: 0,
-      startPage: 1,
-      endPage: 5,
-      sectionRoles: 'CANDIDATE',
-      headers: [],
-    };
-  }
+import url from "../../utils/UrlConstant";
+const SectionList = () => {
+  const [sections, setSections] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [openModalAdd, setOpenModalAdd] = useState(false);
+  const [modalSection, setModalSection] = useState('');
+  const [status, setStatus] = useState('ACTIVE');
+  const [loader, setLoader] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalPages, setTotalPages] = useState(0);
+  const [totalElements, setTotalElements] = useState(0);
+  const [numberOfElements, setNumberOfElements] = useState(0);
+  const [startPage, setStartPage] = useState(1);
+  const [endPage, setEndPage] = useState(5);
+  const [headers, setHeaders] = useState([]);
 
-  componentDidMount() {
-    this.setTableJson()
-    this.setState({ loader: true });
-    axios.get(` ${url.ADMIN_API}/section/list?sectionRoles=${this.state.sectionRoles}&status=${this.state.status}&page=${this.state.currentPage}&size=${this.state.pageSize}`, {
-      headers: authHeader(),
-    })
-      .then((res) => {
-        this.setState({ sections: res.data.response.content, loader: false, totalPages: res.data.response.totalPages, totalElements: res.data.response.totalElements, numberOfElements: res.data.response.numberOfElements });
-      })
-      .catch((error) => {
-        errorHandler(error);
-        this.setState({ loader: false });
-      });
-  }
+  useEffect(() => {
+    initialCall();
+  }, [currentPage, pageSize, status]);
 
-  handleStatusFilter(value, key) {
-    this.setState({ status:value });
+  const initialCall = () => {
+    setHeader();
+    setLoader(true);
+
     axios
-      .get(` ${url.ADMIN_API}/section/list?sectionRoles=${this.state.sectionRoles}&${key}=${value}&page=${this.state.currentPage}&size=${this.state.pageSize}`, {
+      .get(`${url.ADMIN_API}/section/list?status=${status}&page=${currentPage}&size=${pageSize}`, {
         headers: authHeader(),
       })
       .then((res) => {
-        this.setState({ sections: res.data.response.content, totalPages: res.data.response.totalPages, totalElements: res.data.response.totalElements, numberOfElements: res.data.response.numberOfElements });
+        setSections(res.data.response.content);
+        setLoader(false);
+        setTotalPages(res.data.response.totalPages);
+        setTotalElements(res.data.response.totalElements);
+        setNumberOfElements(res.data.response.numberOfElements);
       })
       .catch((error) => {
+        setLoader(false);
         errorHandler(error);
       });
-  }
-
-  onClickOpenModalAdd = () => {
-    if (!this.state.openModalAdd) {
-      document.addEventListener('click', this.handleOutsideClick, false);
-    } else {
-      document.removeEventListener('click', this.handleOutsideClick, false);
-    }
-    this.setState({ openModalAdd: !this.state.openModalAdd });
   };
 
-  onClickOpenModel = (data) => {
-    if (!this.state.openModal) {
-      document.addEventListener("click", this.handleOutsideClick, false);
-    } else {
-      document.removeEventListener("click", this.handleOutsideClick, false);
-    }
-    this.setState({ openModal: !this.state.openModal });
-    this.setState({ modalSection: data });
-  };
-
-  handleOutsideClick = (e) => {
-    if (e.target.className === "modal fade show") {
-      if (this.state.openModal === true) {
-        this.setState({ openModal: !this.state.openModal });
-        this.componentDidMount();
-      }
-      else {
-        this.setState({ openModalAdd: !this.state.openModalAdd });
-        this.componentDidMount();
-      }
-    }
-  };
-
-  onCloseModalAdd = () => {
-    this.setState({ openModalAdd: !this.state.openModalAdd });
-    this.componentDidMount();
-  };
-
-  onCloseModal = () => {
-    this.setState({ openModal: !this.state.openModal });
-    this.componentDidMount();
-  };
-
-  onNextPage = () => {
-    axios.get(` ${url.ADMIN_API}/section/list/?sectionRoles=${this.state.sectionRoles}&status=${this.state.status}&page=${this.state.currentPage}&size=${this.state.pageSize}`, { headers: authHeader() })
-      .then(res => {
-        this.setState({ sections: res.data.response.content, totalPages: res.data.response.totalPages, totalElements: res.data.response.totalElements, numberOfElements: res.data.response.numberOfElements });
-      })
-  }
-
-  increment = (event) => {
-    this.setState({
-      startPage: (this.state.startPage) + 5,
-      endPage: (this.state.endPage) + 5
-    });
-  }
-  decrement = (event) => {
-    this.setState({
-      startPage: (this.state.startPage) - 5,
-      endPage: (this.state.endPage) - 5
-    });
-  }
-
-  onPagination = (pageSize, currentPage) => {
-    this.setState({ pageSize: pageSize, currentPage: currentPage }, () => { this.onNextPage() });
-  }
-  setTableJson = () => {
-    const headers = [
-      {
-        name: 'S.NO',
-        align: 'center',
-        key: 'S.NO',
-      },
-      {
-        name: 'NAME',
-        align: 'left',
-        key: 'name',
-      },
-      {
-        name: 'DESCRIPTION',
-        align: 'left',
-        key: 'description',
-      },
+  const setHeader = () => {
+    const newHeaders = [
+      { name: 'S.NO', align: 'center', key: 'S.NO' },
+      { name: 'NAME', align: 'left', key: 'name' },
+      { name: 'DESCRIPTION', align: 'left', key: 'description' },
       {
         name: 'STATUS',
         align: 'left',
@@ -150,117 +60,168 @@ export default class SectionList extends Component {
         key: 'status',
         renderOptions: () => {
           return _.map([{ name: 'Active', value: 'ACTIVE' }, { name: 'InActive', value: 'INACTIVE' }], (opt) => (
-            <CustomMenuItem onClick={() => this.handleStatusFilter(opt.value, 'status')} key={opt.value} value={opt.value}>
+            <CustomMenuItem onClick={() => handleStatusFilter(opt.value, 'status')} key={opt.value} value={opt.value}>
               {opt.name}
             </CustomMenuItem>
           ));
         },
       },
       {
-        name: 'Action',
-        key: 'action',
-        renderCell: (params) => {
-          return (
-            <i
+        name: 'ACTION',
+        align: 'center',
+        renderCell: (params) => (
+          <i
             className="fa fa-pencil"
             style={{ cursor: 'pointer', color: '#3B489E' }}
             aria-hidden="true"
-            onClick={() =>this.onClickOpenModalAdd(params)}
+            onClick={() => onClickOpenModel(params)}
           ></i>
-          );
-        },
+        ),
       },
-    ]
-    this.setState({ headers: headers });
+    ];
+
+    setHeaders(newHeaders);
   };
 
+  const onPagination = (pageSize, currentPage) => {
+    setPageSize(pageSize);
+    setCurrentPage(currentPage);
+    onNextPage();
+  };
 
+  const handleStatusFilter = (value, key) => {
+    setStatus(value);
 
-  render() {
-    let i = this.state.pageSize - 1;
-    return (
-      <main className="main-content bcg-clr">
-        <div>
-          <div className="card-header-new">
-            <span>Section List</span>
-            <button type="button" onClick={this.onClickOpenModalAdd} className="btn btn-nxt btn-sm header-button">Add Section</button>
-            {this.state.openModalAdd ? <SectionModal modalSection={{ action: "Add", section: this.state.modalSection, }} onCloseModalAdd={this.onCloseModalAdd} /> : null} </div>
-          {/* <div className="row">
-            <div className="col-md-12">
-              <div className="table-border">
-                <div>
-                  {fallBackLoader(this.state.loader)}
-                  <div className="table-responsive pagination_table">
-                    <table className="table table-striped">
-                      <thead className="table-dark">
-                        <tr>
-                          <th>S.No</th>
-                          <th>NAME</th>
-                          <th>DESCRIPTION</th>
-                          <th>
-                            <div className="row">
-                              STATUS
-                              <div className="col-sm">
-                                <div className="dropdown">
-                                  <div className="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false" >
-                                    <i className="fa fa-filter" aria-hidden="true"></i> </div>
-                                  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton" >
-                                    <option className="dropdown-item" onClick={(e) => this.handleStatusFilter(e, "status")} value="ACTIVE" > Active </option>
-                                    <option className="dropdown-item" onClick={(e) => this.handleStatusFilter(e, "status")} value="INACTIVE" > InActive </option>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </th>
-                          <th>ACTION</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {this.state.sections?.length > 0 ? (
-                          _.map(this.state.sections, (section, index) => {
-                            return (
-                              <tr>
-                                <td>{this.state.pageSize * this.state.currentPage - (i--)}</td>
-                                <td>{section.name}</td>
-                                <td>{section.description}</td>
-                                <td
-                                  className={section.status === "INACTIVE" ? "text-danger" : "text-success"} > {section.status} </td> <td>
-                                  <button type="button" onClick={() => this.onClickOpenModel(section)} className="btn" >
-                                    <i className="fa fa-pencil" aria-hidden="true" ></i> </button>
-                                </td>
-                              </tr>);
-                          })) : (<tr className="text-center"> <td colspan="6">No data available in table</td> </tr>)}
-                      </tbody>
-                    </table>
-                    {this.state.numberOfElements === 0 ? '' :
-                      <Pagination totalPages={this.state.totalPages} currentPage={this.state.currentPage}
-                        onPagination={this.onPagination} increment={this.increment} decrement={this.decrement}
-                        startPage={this.state.startPage} numberOfElements={this.state.numberOfElements} endPage={this.state.endPage}
-                        totalElements={this.state.totalElements} pageSize={this.state.pageSize} />}
-                  </div>
-                  {this.state.openModal ? <SectionModal modalSection={{ action: "Update", section: this.state.modalSection, }} onCloseModal={this.onCloseModal} /> : null}
-                </div>
-              </div>
-            </div>
-          </div> */}
-          <CustomTable headers={this.state.headers} data={this.state.sections} pageSize={this.state.pageSize} currentPage={this.state.currentPage} />
-          {this.state.numberOfElements === 0 ? '' :
-            <Pagination
-              totalPages={this.state.totalPages}
-              currentPage={this.state.currentPage}
-              onPagination={this.onPagination}
-              increment={this.increment}
-              decrement={this.decrement}
-              startPage={this.state.startPage}
-              numberOfElements={this.state.numberOfElements}
-              endPage={this.state.endPage}
-              totalElements={this.state.totalElements}
-              pageSize={this.state.pageSize}
+    axios
+      .get(`${url.ADMIN_API}/section/list?${key}=${value}&page=${currentPage}&size=${pageSize}`, {
+        headers: authHeader(),
+      })
+      .then((res) => {
+        setLoader(false);
+        setSections(res.data.response.content);
+        setTotalPages(res.data.response.totalPages);
+        setTotalElements(res.data.response.totalElements);
+        setNumberOfElements(res.data.response.numberOfElements);
+      })
+      .catch((error) => {
+        setLoader(false);
+        errorHandler(error);
+      });
+  };
 
-            />}
-        </div>
-      </main>
-    );
-  }
+  const onClickOpenModalAdd = () => {
+    if (!openModalAdd) {
+      document.addEventListener('click', handleOutsideClick, false);
+    } else {
+      document.removeEventListener('click', handleOutsideClick, false);
+    }
+    setOpenModalAdd(true);
+  };
+
+  const onClickOpenModel = (data) => {
+    if (!openModal) {
+      document.addEventListener('click', handleOutsideClick, false);
+    } else {
+      document.removeEventListener('click', handleOutsideClick, false);
+    }
+    setOpenModal(true);
+    setModalSection(data);
+  };
+
+  const handleOutsideClick = (e) => {
+    if (e.target.className === 'modal fade show') {
+      if (openModal) {
+        setOpenModal(false);
+        initialCall();
+      } else {
+        setOpenModalAdd(false);
+        initialCall();
+      }
+    }
+  };
+
+  const onCloseModalAdd = () => {
+    setOpenModalAdd(false);
+    initialCall();
+  };
+
+  const onCloseModal = () => {
+    setOpenModal(false);
+    initialCall();
+  };
+
+  const onNextPage = () => {
+    axios
+      .get(`${url.ADMIN_API}/section/list/?&status=${status}&page=${currentPage}&size=${pageSize}`, {
+        headers: authHeader(),
+      })
+      .then((res) => {
+        setSections(res.data.response.content);
+        setTotalPages(res.data.response.totalPages);
+        setTotalElements(res.data.response.totalElements);
+        setNumberOfElements(res.data.response.numberOfElements);
+        setLoader(false);
+      })
+      .catch((error) => {
+        setLoader(false);
+        errorHandler(error);
+      });
+  };
+
+  const increment = (_event) => {
+    setStartPage((prevStartPage) => prevStartPage + 5);
+    setEndPage((prevEndPage) => prevEndPage + 5);
+  };
+
+  const decrement = (_event) => {
+    setStartPage((prevStartPage) => prevStartPage - 5);
+    setEndPage((prevEndPage) => prevEndPage - 5);
+  };
+
+  return (
+    <main className="main-content bcg-clr">
+    <div>
+      <div className="card-header-new">
+        <span>Sections</span>
+        <button type="button" onClick={onClickOpenModalAdd} className="btn btn-nxt btn-sm header-button">Add Section</button>
+        {openModalAdd ? (
+          <SectionModal modalSection={{ action: "Add", section: modalSection, }} onCloseModalAdd={onCloseModalAdd} />
+        ) : ("")}
+      </div>
+      <CustomTable data={sections}
+        headers={headers}
+        loader={loader}
+        pageSize = {pageSize}
+        currentPage = {currentPage}
+      />
+      {numberOfElements === 0 ? '' :
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPagination={onPagination}
+          increment={increment}
+          decrement={decrement}
+          startPage={startPage}
+          numberOfElements={numberOfElements}
+          endPage={endPage}
+          totalElements={totalElements}
+          pageSize={pageSize}
+
+        />}
+      {openModal ? (
+        <SectionModal
+          modalSection={{
+            action: "Update",
+            section: modalSection,
+          }}
+          onCloseModal={onCloseModal}
+        />
+      ) : (
+        ""
+      )}
+    </div>
+  </main>
+  )
 }
+
+export default SectionList
